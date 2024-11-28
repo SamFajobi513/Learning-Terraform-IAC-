1f8d636004d218c3eb86ab97ae782cf8bb4d4d21
SamFajobi <olusegunfajobi12@gmail.com>
2024-11-28 17:35:09 +0100

    blocksAndAttributes

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
 #Learning HCL 
 
 #modules
+
+#Blocks(this serves as  a container for other content like the attributes..)
+block_type "" "" {
+    attributes = "value 1"
+    attributes = "value 2"
+}
+
+resource "aws" "" {
+    ami = "1334"
+    instancetype = "t2.macro"
+    count = 4
+    enabled = true
+}
+
+#Attributes
+
+
+#DataTypes
+#string
+
+#Lists(this is defined in a bracket)
+security_groups = ["hj", "book"]
+
